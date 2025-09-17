093bc97d00a378a047aea56f1f56a5be14af323d
Micael <162610600+MicaelGimenez@users.noreply.github.com>
2025-09-17 11:43:29 -0300

    plantas.sql

--- MySQL/plantas.sql
+++ MySQL/plantas.sql
@@ -1,6 +1,7 @@
 CREATE TABLE plantas (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     humedad VARCHAR(20),
+    porcentaje DECIMAL(5,2) CHECK (porcentaje >= 0 AND porcentaje <= 100),
     PRIMARY KEY (id)
 );
